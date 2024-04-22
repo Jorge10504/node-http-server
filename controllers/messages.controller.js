19ae2de72ea1d40dd20d1d94aca8fa8f3fcda8cd
Jorge10504 <georgeunal2016@gmail.com>
2024-04-22 16:18:49 -0500

    Serving static html files and images

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,8 +1,7 @@
 const path = require("path");
 
 function getMessages(req, res) {
-  res.sendFile(path.join(__dirname, "..", "public", "skimountain.jpg"));
-  // res.send("<ul><li>Hello Albert Einstein!</li></ul>");
+  res.sendFile(path.join(__dirname, "..", "public", "imgs", "skimountain.jpg"));
 }
 
 function postMessage(req, res) {
